77da20403d1505c819f181b9e54aa792567f8d9d
Hugues <hugues.devimeux@epfl.ch>
2023-06-21 15:12:27 +0200

    testing new theme

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "myst_nb",
+    "sphinx_rtd_theme"
 ]
 # pip install myst-nb
 
@@ -155,7 +156,7 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
